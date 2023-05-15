cddead9164eb9c1c29733fcde1e354a05b60d6d7
ThicoProgrammer <102100201+ThicoProgrammer@users.noreply.github.com>
2023-05-15 19:09:04 -0300

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+
+import {
+    Text,
+    View
+} from "react-native"
+
+export default class FeedScreen extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Criar Feed</Text>
+            </View>
+        )
+    }
+}
